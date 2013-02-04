cfe88eb8d5042d03b0cc75d3969f01f123150b37
Aaron Gitlin <aarongitlin@gmail.com>
2013-02-04 18:22:31 +1100

    model for settings, modified to attach with slides

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -4,4 +4,8 @@ class Setting < ActiveRecord::Base
                   :wednesday, :thursday, :friday, :saturday, :sunday, :logo
 
   has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+
+  attr_accessible :title, :content, :slides_attributes
+  has_many :slides
+  accepts_nested_attributes_for :slides, :allow_destroy => true 
 end
